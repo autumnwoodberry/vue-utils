c4f7aa74cf3fd550cef51f0b0e6334f2847c4d02
Adam Woodbury <adamwoodbury@gmail.com>
2018-03-16 00:38:34 -0700

    Add special-input component

---
+++ src/components/special-input.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="special-input">
+    <input v-bind="$attrs" v-on="$listeners">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'special-input'
+}
+</script>
+
+<style scoped>
+.special-input {
+  background-color: purple;
+  padding: 1em;
+}
+</style>
